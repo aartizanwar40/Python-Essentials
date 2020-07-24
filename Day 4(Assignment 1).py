8195739332560299eaa18fc0501fa0c24b6b5c6a
aartizanwar40 <63449239+aartizanwar40@users.noreply.github.com>
2020-07-24 14:52:16 +0530

    Add files via upload

---
+++ Day 4(Assignment 1).py
@@ -0,0 +1,6 @@
+#ASSIGNMENT "FIND ALL OCCURENCES OF SUBSTRING IN THE GIVEN STRING"
+#What we think we became;we are python programmer"   print index vvalues of "we"
+
+str1 = "what we think we became;we are python programmer"
+print(str1.count("we"))
+print(str1.index("we"))
---
+++ Day 4(Assignment 2).py
@@ -0,0 +1,15 @@
+#Assignment 2:----
+# islower() ---> with different kind of strngs
+# isupper()
+str1 = "HELLO EVERYONE!!!! HOW ARE YOU!!"
+print(str1.islower())
+print(str1.isupper())
+
+str2 = "heyy i love learning python with letsupgrade"
+print(str2.islower())
+print(str2.isupper())
+
+str3 = "Heyy Evenryone!! This is my Day 4 assignment of Python Essentials"
+print(str3.islower())
+print(str3.isupper())
+
